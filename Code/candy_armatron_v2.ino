6f034f505f0229a969709310ace43c41900b6029
Professor Zia <68821049+professor-zia@users.noreply.github.com>
2025-10-17 22:06:48 -0400

    Add files via upload

--- Code/candy_armatron_v2.ino
+++ Code/candy_armatron_v2.ino
@@ -22,7 +22,7 @@ int servo1Pin = 16, servo2Pin = 17, servo3Pin = 18, servo4Pin = 19;
 int analogJ1XPin = 33, analogJ1YPin = 32, analogJ2XPin = 34, analogJ2YPin = 35;
 
 // global variables to store current servo positions, initialize to mid positions (90 deg)
-int servo1Pos = 90, servo2Pos = 90, servo3Pos = 90, servo4Pos = 30;
+int servo1Pos = 90, servo2Pos = 90, servo3Pos = 90, servo4Pos = 45;
 
 void setup() {
   // initialize serial communication at 115200 bits per second:
@@ -49,7 +49,7 @@ void setup() {
   // default min/max is 1000us and 2000us
   // using min/max of 500us and 2400us for micro servos used in arm-a-tron
   // for an accurate 0 to 180 sweep
-  armServo1.attach(servo1Pin, 500, 2400);
+  armServo1.attach(servo1Pin, 500, 2500);
   armServo2.attach(servo2Pin, 500, 2400);
   armServo3.attach(servo3Pin, 500, 2400);
   armServo4.attach(servo4Pin, 500, 2400);
@@ -71,13 +71,13 @@ void loop() {
 
   // control servos by mapping 12-bit (0-4095) analog values ...
   // to servos positions (0-180 degrees)
-  servo1Pos = map(analogValue1, 0, 1024, 180, 0);
+  servo1Pos = map(analogValue1, 0, 1024, 0, 180);
   armServo1.write(servo1Pos);
 
-  servo2Pos = map(analogValue2, 0, 1024, 180, 0);
+  servo2Pos = map(analogValue2, 0, 1024, 0, 180);
   armServo2.write(servo2Pos);
 
-  servo3Pos = map(analogValue3, 0, 1024, 0, 180);
+  servo3Pos = map(analogValue3, 0, 1024, 180, 0);
   armServo3.write(servo3Pos);
 
 if (analogValue4 < 400) servo4Pos = 0;
